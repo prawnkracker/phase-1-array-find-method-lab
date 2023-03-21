0162e76b802deeb0441e9be7b02f5cc877953051
Dennis Demir <dennisjdemir@gmail.com>
2023-03-20 21:35:59 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    const win = array.find(function(game){ 
+        return game.result === 'W'
+    })
+    return win ? win.year : undefined;
+}
